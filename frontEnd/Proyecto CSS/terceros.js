33b13a3c131ebdacfa691a0ad36eab608741177a
Sectario <trcesarcamilo@gmail.com>
2022-10-29 22:27:28 -0500

    implementaciion del fetch obtener datos de la api

--- frontEnd/Proyecto CSS/terceros.js
+++ frontEnd/Proyecto CSS/terceros.js
@@ -5,12 +5,82 @@ const btnCerrarModal=document.querySelector("#btn-cerrar-modal");
 const modal =document.querySelector("#modal");
 
 
-fetch(url)
-    .then(response => response.json() )   
+//Function fetch
+
+fetchfunction = () => {fetch(url)
+    .then(response => response.json() )
     .then(data =>{
         console.log(data)
+        createTable(data.results)
     })
     .catch(err=>console.log(err))
+}
+
+fetchfunction()
+
+//ingresar datos a la tabla de la función fetc
+
+createTable = (results) => {
+   
+    console.log(results)    
+    for (item of results) {
+        console.log(item.name)
+
+        const tr=document.createElement("tr");
+        
+        const nombre  = item.name
+        const tdNombre=document.createElement("td");
+        let txt=document.createTextNode(nombre);
+        tdNombre.appendChild(txt);
+        tdNombre.className="nombre";
+
+
+        const telefono  = item.id
+        const tdTelefono=document.createElement("td")
+        txt=document.createTextNode(telefono);
+        tdTelefono.appendChild(txt);
+        tdTelefono.className="telefono";
+
+        const email  = item.status
+        const tdEmail=document.createElement("td");
+        txt=document.createTextNode(email);
+        tdEmail.appendChild(txt);
+        tdEmail.className="email";
+
+        const direccion  = item.species
+        const tdDireccion=document.createElement("td");
+        txt=document.createTextNode(direccion);
+        tdDireccion.appendChild(txt);
+        tdDireccion.className="direccion";
+
+        const usuario = item.name
+        const tdUsuario=document.createElement("td");
+        txt=document.createTextNode(usuario);
+        tdUsuario.appendChild(txt);
+        tdUsuario.className="usuario";
+
+        const tdRemove=document.createElement("td");
+        const buttonRemove=document.createElement("img");
+        buttonRemove.src="";
+        buttonRemove.onclick=eliminarFila;
+        tdRemove.appendChild(buttonRemove);
+
+        tr.appendChild(tdNombre);
+        tr.appendChild(tdTelefono);
+        tr.appendChild(tdEmail);
+        tr.appendChild(tdDireccion);
+        tr.appendChild(tdUsuario);
+        tr.appendChild(tdRemove);
+
+        const tbody=document.getElementById("proveedor").querySelector("tbody").appendChild(tr);
+
+    // eliminamos la clase que tiene oculta la tabla cando no hay ningun alumno
+    document.getElementById("proveedor").classList.remove("hide");
+    }
+}
+
+
+
 
 
 btnAbrirModal.addEventListener('click',()=>{
@@ -31,7 +101,7 @@ document.querySelector("input[type=submit]").addEventListener("click",function(e
 
     agregarFila(nombre.value, telefono.value, email.value, direccion.value, usuario.value);
     agregarInput(nombre.value, telefono.value, email.value, direccion.value, usuario.value);
- 
+
     //limpiamos los valores del input
     nombre.value="";
     telefono.value="";
@@ -48,7 +118,7 @@ document.querySelector("input[type=submit]").addEventListener("click",function(e
 function agregarFila(nombre, telefono,email,direccion,usuario) {
     // añadimos el alumno a la tabla crando el tr, td's y el botón para eliminarlo
     const tr=document.createElement("tr");
- 
+
     const tdNombre=document.createElement("td");
     let txt=document.createTextNode(nombre);
     tdNombre.appendChild(txt);
@@ -58,12 +128,12 @@ function agregarFila(nombre, telefono,email,direccion,usuario) {
     txt=document.createTextNode(telefono);
     tdTelefono.appendChild(txt);
     tdTelefono.className="telefono";
- 
+
     const tdEmail=document.createElement("td");
     txt=document.createTextNode(email);
     tdEmail.appendChild(txt);
     tdEmail.className="email";
-    
+
     const tdDireccion=document.createElement("td");
     txt=document.createTextNode(direccion);
     tdDireccion.appendChild(txt);
@@ -79,15 +149,15 @@ function agregarFila(nombre, telefono,email,direccion,usuario) {
     buttonRemove.src="";
     buttonRemove.onclick=eliminarFila;
     tdRemove.appendChild(buttonRemove);
- 
-      
+
+
     tr.appendChild(tdNombre);
     tr.appendChild(tdTelefono);
     tr.appendChild(tdEmail);
     tr.appendChild(tdDireccion);
     tr.appendChild(tdUsuario);
     tr.appendChild(tdRemove);
- 
+
     const tbody=document.getElementById("proveedor").querySelector("tbody").appendChild(tr);
 
     // eliminamos la clase que tiene oculta la tabla cando no hay ningun alumno
@@ -100,10 +170,10 @@ function eliminarFila(e) {
     const email=tr.querySelector(".email").innerText;
     const direccion=tr.querySelector(".direccion").innerText;
     const usuario=tr.querySelector(".usuario").innerText;
- 
+
     eliminarInput(nombre, telefono,email,direccion,usuario);
     tr.remove();
- 
+
     // Si no hay ningun elemento en la lista, escondemos la tabla
     if (document.getElementById("proveedor").querySelector("tbody").querySelectorAll("tr").length==0) {
         document.getElementById("proveedor").classList.add("hide");
@@ -115,7 +185,7 @@ function agregarInput(nombre, telefono,email,direccion,usuario) {
     input.type="hidden";
     input.name="nombres[]";
     input.value=nombre+"-"+telefono+"-"+email+"-"+direccion+"-"+usuario;
- 
+
     document.querySelector("form").appendChild(input);
 }
 
